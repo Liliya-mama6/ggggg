10e542d2c4fb954002529f591208d4cbb19aa8c2
Liliya-mama6 <vozdvizhenka@mail.ru>
2024-09-22 13:00:32 +0500

    Update real.p

--- real.py
+++ real.py
@@ -1,22 +1,10 @@
-def sum_all(a, b):
-    try:
-        c=False
-        for i in range(len(a)):
-            if a[i]=='.':
-                c=True
-        if c:
-            a=float(a)
-        else:
-            a=int(a)
-        c = False
-        for i in range(len(b)):
-            if b[i] == '.':
-                c = True
-        if c:
-            b = float(b)
-        else:
-            b = int(b)
-        return a+b
-    except:
-        return str(a)+str(b)
-print(sum_all(input(), input()))
+
+def all_variants(a):
+    i=1
+    while i<=len(a):
+        for j in range(len(a)-i+1):
+            yield a[j:j+i]
+        i+=1
+store=all_variants(input())
+for i in store:
+    print(i)
