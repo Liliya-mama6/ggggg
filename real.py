46a98331082ba5acb77d59d58df1b0a1e7a49be7
Liliya-mama6 <vozdvizhenka@mail.ru>
2024-10-20 21:49:43 +0500

    Update real.p

--- real.py
+++ real.py
@@ -1,10 +1,13 @@
+'''библиотека Pillow позволяет удобно работать с изображениями. Главной помощью в этом является класс Image
+и его методы. например этот класс с помощью метода формат позволяет нам конвертировать изображение в другой
+тип данных. С помощью size мы можем изменить размер фаайла. с помощью crop мы можем вырезать, обьединять.
+для работы с изображениями оень удобен.'''
 
-def all_variants(a):
-    i=1
-    while i<=len(a):
-        for j in range(len(a)-i+1):
-            yield a[j:j+i]
-        i+=1
-store=all_variants(input())
-for i in store:
-    print(i)
+from PIL import Image
+
+cartinca = Image.open('xh.jpeg')
+l = cartinca.crop((25, 25, 75, 75))
+l.show()  # просмаатриваать или легко нааходить изображения
+print(cartinca.format, cartinca.size, cartinca.mode)  # gросмотр хаарактеристик
+cartinca.resize((10, 10))  # изменение размера
+cartinca.save('resized.jpeg')  # и конвертирование
