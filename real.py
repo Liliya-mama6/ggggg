01b052054e3bda319d7907e8045c8e511a153f07
Liliya-mama6 <vozdvizhenka@mail.ru>
2024-11-23 12:32:58 +0500

    свагер недоступен....

--- real.py
+++ real.py
@@ -1,26 +1,60 @@
-import sqlite3
+import fastapi
+from pydantic import BaseModel
+from fastapi import Path, HTTPException, Body, Request
+from typing import List
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
-contact = sqlite3.connect('dt.db')
-cursor = contact.cursor()
 
-cursor.execute('''
-CREATE TABLE IF NOT EXISTS User(
-id INTEGER PRIMARY KEY,
-username TEXT NOT NULL,
-email TEXT NOT NULL,
-age INTEGER,
-balance INTEGER NOT NULL
-)
-''')
-cursor.execute('''
-CREATE INDEX IF NOT EXISTS idx_email ON User(email)
-''')
+app = fastapi.FastAPI()
 
-cursor.execute('DELETE FROM User WHERE id = ?', (6,))
-cursor.execute('SELECT COUNT(*) FROM User')
-a = (cursor.fetchall()[0][0])
-cursor.execute('SELECT SUM(balance) FROM User')
-b = int(cursor.fetchall()[0][0])
-print(b / a)
-contact.commit()
-contact.close()
+# $ python3 -m uvicorn iscandar:app
+users = []
+
+a=Jinja2Templates(directory='templates')
+
+
+class User(BaseModel):
+    id: int = None
+    nick: str
+    year: int
+
+
+@app.get('/')
+async def welcome(request:Request) -> HTMLResponse:
+    return a.TemplateResponse('users.html', {'request':request, 'users':users})
+
+
+@app.get('/user/{user_id}')
+async def get(request:Request, user_id:int) -> HTMLResponse:
+    try:
+        return a.TemplateResponse('users.html', {'request':request, 'user':users[user_id]})
+    except IndexError:
+        raise HTTPException(status_code=404, detail='User not found')
+
+@app.post(path='/user/{username}/{age}')
+async def create(username: str, age: int) -> User:
+    user: User = User(id=len(users) + 1, nick=username, year=age)
+    users.append(user)
+    return user
+
+
+@app.put('/user/{user_id}/{username}/{age}')
+async def update(user_id, username, age) -> str:
+    try:
+        a:User=User(id=user_id, nick=username, year=age)
+        users[user_id] = a
+        return 'all was successful'
+    except IndexError:
+        raise HTTPException(status_code=404, detail="User was not found")
+
+
+@app.delete('/user/{user_id}')
+async def delete(user_id) -> str:
+    global users
+    try:
+        new_users = users[:user_id - 1] + users[user_id:]
+        users = new_users
+        return 'User was deleted'
+    except:
+        HTTPException(status_code=404, detail="User was not found")
